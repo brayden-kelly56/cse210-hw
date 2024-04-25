6b689116ffb7ecdb3d731774df4f420edd6598e8
Brayden Kelly <kel.20012@byui.edu>
2024-04-25 15:13:14 -0600

    This is the test we did in class

--- sandbox/Ollie/Program.cs
+++ sandbox/Ollie/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+// Such comment many wow
+//Get input
+string str;
+Console.Write('What the dog doin?');
+str = Console.ReadLine();
+//Display output
+Console.WriteLine(str);
+//Variable types
+int i;
+double f;
+bool tf;
+// Math stuff "+ - * / %"
+// && 'and'
+// || 'or'
+// ! 'not'
