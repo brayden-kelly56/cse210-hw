32540e09f289395924455e43353ce6498742d66a
Brayden Kelly <kel.20012@byui.edu>
2024-04-27 22:41:03 -0600

    Work for assignment number 2.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your current grade percentage? ");
+        string input = Console.ReadLine();
+        int percentage = int.Parse(input);
+
+        if (percentage >= 90)
+        {
+            Console.WriteLine("Your current grade is an A");
+        }
+        else if (percentage >= 80)
+        {
+            Console.WriteLine("Your current grade is an B");
+        }
+        else if (percentage >= 70)
+        {
+            Console.WriteLine("Your current grade is an C");
+        }
+        else if (percentage >= 60)
+        {
+            Console.WriteLine("Your current grade is an D");
+        }
+        else
+        {
+            Console.WriteLine("Your current grade is an F");
+        }
     }
 }
