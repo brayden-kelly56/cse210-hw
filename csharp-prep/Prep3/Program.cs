475bedd2d7ab77d76937fa69252b2008a6a0c0fa
Brayden Kelly <kel.20012@byui.edu>
2024-05-01 12:00:06 -0600

    Random number game project is complete!

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        bool finish = false;
+        Random num = new Random();
+        int magicNumber = num.Next(1,100);
+        while (finish == false)
+        {
+            Console.WriteLine("What is your guess? ");
+            string guessInput = Console.ReadLine();
+            int guess = int.Parse(guessInput);
+            if (guess == magicNumber)
+            {
+                finish = true;
+                Console.WriteLine("You guessed it!");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+        }
     }
 }
