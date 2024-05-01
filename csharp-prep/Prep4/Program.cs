8f9c5945917c3d2219691d7950ac839ab197fb88
Brayden Kelly <kel.20012@byui.edu>
2024-05-01 12:24:16 -0600

    Completed number list program.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,43 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new();
+        bool cont = true;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        do
+        {
+            Console.WriteLine("Enter Number: ");
+            string numInput = Console.ReadLine();
+            int num = int.Parse(numInput);
+            if (num == 0)
+            {
+                cont = false;
+            }
+            else
+            {
+                numbers.Add(num);
+            }
+        } while (cont == true);
+
+        int sum = 0;
+        int largest = 0;
+        int tally = 0;
+        foreach (int value in numbers)
+        {
+            sum += value;
+            tally++;
+            if (value >= largest)
+            {
+                largest = value;
+            }
+        }
+        
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {(double)sum / tally}");
+        Console.WriteLine($"The largest number is: {largest}");
     }
 }
