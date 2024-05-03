9e05d08a02698a97639f232f59b5616d06aa3888
Brayden Kelly <kel.20012@byui.edu>
2024-05-03 13:11:30 -0600

    Completion of function based program.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,39 @@
 using System;
+using System.Globalization;
 
 class Program
 {
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+    static string PromptUserName()
+    {
+        Console.WriteLine("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+    static int PromptUserNumber()
+    {
+        Console.WriteLine("Please enter your favorite number:");
+        int Number = Console.Read();
+        return Number;
+    }
+    static int SquareNumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+    static void DisplayResult(int square, string name)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int square = SquareNumber(number);
+        DisplayResult(square, name);
     }
 }
