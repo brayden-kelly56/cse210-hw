76421ecc7ec49836b697397f86ae1c98f696b342
Brayden Kelly <kel.20012@byui.edu>
2024-05-22 10:03:32 -0600

    Progress on Journal program.

--- prove/Develop02/Menu.cs
+++ prove/Develop02/Menu.cs
@@ -1,4 +1,16 @@
 public class Menu
 {
+    //attributes
 
+    //methods
+    public void put_menu()
+    {
+        Console.WriteLine("Welcome to the Journal Program!");
+        Console.WriteLine("Please choose one of the following options");
+        Console.WriteLine("1. Write");
+        Console.WriteLine("2. Display");
+        Console.WriteLine("3. Load");
+        Console.WriteLine("4. Save");
+        Console.WriteLine("5. Quit");
+    }
 }
